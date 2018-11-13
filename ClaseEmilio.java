aeddba64c9d13b990b38a75b2d1c6c84b0411349
emendezduran <emendezduran@nautilus25.danielcastelao.org>
2018-11-13 10:21:53 +0100

    commit

---
+++ ClaseEmilio.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author emendezduran
+ */
+public class ClaseEmilio {
+    //El puto amo
+}
